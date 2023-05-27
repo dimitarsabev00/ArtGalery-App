262691272be02338521ea1c4972d566f62196763
Dimitar Sabev <dimitarsabev00@gmail.com>
2023-05-27 16:13:20 +0300

    Added ProgressBar Component

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const UploadForm = () => {
   const [file, setFile] = useState(null);
@@ -28,6 +29,7 @@ const UploadForm = () => {
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
+        { file && <ProgressBar file={file} setFile={setFile} /> }
       </div>
     </form>
   );
